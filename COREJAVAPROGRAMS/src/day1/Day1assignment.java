431cdc201e477ac4e8ee59c8f8d4bf2f77302031
Devadharshini B <Devadharshini B@LAPTOP-5SM4DH8O>
2025-09-23 06:45:27 +0530

    commit

---
+++ COREJAVAPROGRAMS/src/day1/Day1assignment.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Day1assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Coders to the Coders world with C2TC");
+
+	}
+
+}
