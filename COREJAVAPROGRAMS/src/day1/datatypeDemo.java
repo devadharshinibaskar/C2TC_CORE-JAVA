5aa26ed14c57e6405fca1a32aa704cd73e0432e1
Devadharshini B <Devadharshini B@LAPTOP-5SM4DH8O>
2025-09-23 21:29:13 +0530

    commit

---
+++ COREJAVAPROGRAMS/src/day1/datatypeDemo.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class datatypeDemo {
+
+	public static void main(String[] args) {
+		int val1=7/3;
+		float val2=72/8;
+		double val3=70d/20d;
+		float val4=6/2.5f;
+		System.out.println("value1=" + val1);
+		System.out.println("value2=" + val2);
+		System.out.println("value3=" + val3);
+		System.out.println("value4=" + val4);
+		
+		int mark_total=512;
+		double percentage=mark_total*0.26f;
+		System.out.println("percentage=" + percentage);
+
+	}
+
+}
