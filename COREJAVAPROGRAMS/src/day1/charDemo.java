28fdb8cc096152f66d1558f4285e935e3d833ddd
Devadharshini B <Devadharshini B@LAPTOP-5SM4DH8O>
2025-09-23 21:11:18 +0530

    commit

---
+++ COREJAVAPROGRAMS/src/day1/charDemo.java
@@ -0,0 +1,24 @@
+package day1;
+
+public class charDemo {
+
+	public static void main(String[] args) {
+		char ch='d';
+		System.out.println(ch);
+		
+		char ch1=67;
+		System.out.println(ch1);
+		
+		char ch2='\u00A7';
+		System.out.println(ch2);
+		
+		char ch3='\u20AC';
+		System.out.println(ch3);
+		
+		int a='D';
+		System.out.println(a);
+		
+
+	}
+
+}
