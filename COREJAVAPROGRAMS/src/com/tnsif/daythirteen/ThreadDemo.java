542b1065609db9e16622cf09da24db3177e53870
devadharshinibaskar <devadharshini.dpy@gmail.com>
2025-11-06 10:09:23 +0530

    commit

---
+++ COREJAVAPROGRAMS/src/com/tnsif/daythirteen/ThreadDemo.java
@@ -0,0 +1,16 @@
+//Executor class for ChildThread
+package com.tnsif.daythirteen;
+
+public class ThreadDemo {
+	public static void main(String[] args) throws InterruptedException {
+		ChildThread threadOne = new ChildThread(5, "First");
+		ChildThread threadTwo = new ChildThread(10, "Second");
+
+		threadOne.start();
+		threadOne.join();
+		threadTwo.start();
+		threadTwo.join();
+
+		System.out.println("-----------------------End of Main--------------------------");
+	}
+}
